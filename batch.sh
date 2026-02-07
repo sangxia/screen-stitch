fbc3ccb0eb1e2fd2606d7bf15633df5094de9ba2
Sangxia Huang <huang.sangxia@gmail.com>
2026-02-07 22:26:27 +0100

    minor batch script tweak

--- batch.sh
+++ batch.sh
@@ -2,5 +2,5 @@
 
 for filename in ./data/*; do
   echo $filename
-  uv run screen-stitch "$filename" out/$(date +%s).png --min-scroll-frac 0.2
+  uv run screen-stitch "$filename" out/$(date +%s).png
 done
