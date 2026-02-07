8517693fd2e67c609f942cff9bd8cc3033c1f47a
sangxia <huang.sangxia@gmail.com>
2026-02-07 22:29:00 +0100

    Update batch script argument handling

--- batch.sh
+++ batch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+for arg in "$@"; do
+  if [[ "$arg" == "-h" || "$arg" == "--help" ]]; then
+    uv run screen-stitch "$@"
+    exit 0
+  fi
+done
+
 for filename in ./data/*; do
-  echo $filename
-  uv run screen-stitch "$filename" out/$(date +%s).png
+  echo "$filename"
+  uv run screen-stitch "$filename" "out/$(date +%s).png" "$@"
 done
